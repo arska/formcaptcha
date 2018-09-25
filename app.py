426d707826175e3e95a42a64298a8e21f64e336f
Aarno Aukia <aarno.aukia@vshn.ch>
2018-09-26 00:30:42 +0300

    redirect on success

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ Show google recaptcha challenge for form submissions matching simple heuristics
 """
 
 import os
-import pprint
-from flask import Flask, request, render_template
-from dotenv import load_dotenv
+import logging
+from flask import Flask, request, render_template, redirect
 from flask_google_recaptcha import GoogleReCaptcha
+from dotenv import load_dotenv
+from werkzeug.urls import url_encode
 
 
 APP = Flask(__name__)  # Standard Flask app
@@ -14,21 +15,36 @@ APP.config["RECAPTCHA_SITE_KEY"] = os.environ.get("RECAPTCHA_SITE_KEY")
 APP.config["RECAPTCHA_SECRET_KEY"] = os.environ.get("RECAPTCHA_SECRET_KEY")
 RECAPTCHA = GoogleReCaptcha(app=APP)
 
+LOGFORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=LOGFORMAT)
+
 
 @APP.route("/", methods=["GET", "POST"])
-def hello():
+def captcha():
     """
-    Hello world on root path
+    test form submission and show captcha. redirect if successful
     """
     if request.method == "GET":
         form = request.args
     else:
         form = request.form
 
+    filteredform = form.copy()
+    filteredform.pop("g-recaptcha-response", None)
+    redirecturl = os.environ.get("REDIRECT_URL", "/") + "?" + url_encode(filteredform)
+
+    # simple heuristic to determine if captcha needs to be shown
+    if (
+        form.get("q1_control_fname", False)
+        and form.get("q3_control_lname", False)
+        and not form.get("q1_control_fname") == form.get("q3_control_lname")
+    ):
+        return redirect(redirecturl)
+
     if form.get("g-recaptcha-response"):
         if RECAPTCHA.verify(response=form.get("g-recaptcha-response")):
-            return "success"
-    return render_template("template.html", form=form)
+            return redirect(redirecturl)
+    return render_template("template.html", form=filteredform)
 
 
 if __name__ == "__main__":
